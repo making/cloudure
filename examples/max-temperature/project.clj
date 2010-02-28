87e5b343262b38be21776af09af504760f8372cd
making <makingx@gmail.com>
2010-03-01 01:43:19 +0900

    add example

---
+++ examples/max-temperature/project.clj
@@ -0,0 +1,18 @@
+(defproject max-temperature "1.0.0-SNAPSHOT" 
+  :description "sample of cloudure" 
+  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+                 [am.ik.cloudure/cloudure "0.1.0-SNAPSHOT"]
+                 [org.apache.mahout.hadoop/hadoop-core "0.20.1"]
+                 [commons-cli/commons-cli "1.2"]
+                 [commons-codec/commons-codec "1.3"]
+                 [commons-el/commons-el "1.0"]
+                 [commons-httpclient/commons-httpclient "3.0.1"]
+                 [commons-logging/commons-logging "1.0.4"]
+                 [commons-logging/commons-logging-api "1.0.4"]
+                 [commons-net/commons-net "1.4.1"]
+                 ]
+  :dev-dependencies [[leiningen/lein-swank "1.1.0"]
+                     ]
+  :main max_temperature
+  )
---
+++ examples/max-temperature/src/max_temperature.clj
@@ -0,0 +1,45 @@
+(ns max-temperature
+  (:gen-class)
+  (:import (org.apache.hadoop.fs Path)
+           (org.apache.hadoop.io IntWritable LongWritable Text)
+           (org.apache.hadoop.mapreduce Job Mapper Mapper$Context Reducer Reducer$Context)
+           (org.apache.hadoop.mapreduce.lib.input FileInputFormat)
+           (org.apache.hadoop.mapreduce.lib.output FileOutputFormat)
+           )
+  (:use am.ik.cloudure.core)
+  )
+
+(defmapreduce temperature 
+  :mapper 
+  ([key value context]
+     (let [line (str value)
+           year (.substring line 15 19)
+           quality (.substring line 92 93)
+           air-temperature (Integer/valueOf 
+                            (.substring line 
+                                        (if (= (.charAt line 87) \+) 88 87)
+                                        92))]
+       (if (and (not (= air-temperature 9999))
+                (.matches quality "[01459]"))
+         (.write context (Text. year) (IntWritable. air-temperature)))
+       ))
+  :reducer
+  ([key values context]
+     (.write context key (IntWritable. (reduce max (map #(.get %) values))))
+     ))
+
+(defn -main [& args]
+  (when (not (= (count args) 2))
+    (.println System/err "args error!")
+    (System/exit -1)
+    )
+  (let [job (get-temperature-job)] ; this job is already set mapper and reducer
+    (FileInputFormat/addInputPath job (Path. (nth args 0)))    
+    (FileOutputFormat/setOutputPath job (Path. (nth args 1)))
+    (doto job      
+      (.setOutputKeyClass Text)
+      (.setOutputValueClass IntWritable)
+      )
+    (System/exit (if (.waitForCompletion job true) 0 1))    
+    )
+  )
